5689888e2e1389b0840a1daa268022292e22e4fd
Tom Anderson <tom@lupin.home>
2014-09-21 12:26:50 -0400

    Initial Commit

---
+++ Homepwner/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Homepwner
+//
+//  Created by Tom Anderson on 9/21/14.
+//  Copyright (c) 2014 Maritom LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
